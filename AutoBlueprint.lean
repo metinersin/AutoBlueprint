e6a9428361666ed84d05b559fd457327016a73fd
Metin Ersin Arıcan <metin.ersin.arican@gmail.com>
2024-08-16 18:06:51 +0300

    initial commit

--- AutoBlueprint.lean
+++ AutoBlueprint.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `AutoBlueprint` library.
 -- Import modules here that should be built as part of the library.
 import AutoBlueprint.Basic
+
+def hello := "Library is working"
